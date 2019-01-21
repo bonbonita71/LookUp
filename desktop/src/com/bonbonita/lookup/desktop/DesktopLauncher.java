006917264cb24e5b78bb1350b479c36fd22c0251
bonbonita71 <bonbonita71@gmail.com>
2019-01-21 22:00:19 +0200

    Created new libGDX project for game LOOK-UP

---
+++ desktop/src/com/bonbonita/lookup/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.bonbonita.lookup.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.bonbonita.lookup.LookUp;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LookUp(), config);
+	}
+}
